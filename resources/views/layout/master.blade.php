61c2afc0e65d21ff11e12ddc5d35cb9befd841a9
EsdrasC24 <josuequeraleswd2420@gmail.com>
2024-07-08 23:22:56 -0400

    feat: add tailwindcss

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,6 +3,8 @@
     <head>
         <title>@yield('')</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
         @include('layout.header')
