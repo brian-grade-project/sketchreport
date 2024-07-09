a98734021f25c61aa6f9d54aa1209124f3a3e6b0
Brian <brianmedina042@gmail.com>
2024-07-09 05:26:40 +0200

    aucilio XD

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -11,7 +11,7 @@
     <header>
         <nav class="navbar bg-base-100 flex">
             <ul class="navbar bg-base-100 flex text-2xl">
-                <li> <a href="#">Inicio</a></li>
+                <li> <a class ="text-red-500" href="#">Inicio</a></li>
                 <li> <a href="#">Información</a></li>
                 <li> <a href="#">Enlaces</a></li>
             </ul>
@@ -20,7 +20,7 @@
 
     <div id="caja1">
 
-        <img class="float-left h-3xl" src="../../public/img/img1.png">
+        <img class="float-left h-xl" src="img/img1.png">
        <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1357 1080"><defs><style>.cls-1{fill:#f96b13;}.cls-2{clip-path:url(#clip-path);}</style><clipPath id="clip-path"><path class="cls-1" d="M433,0,233.42.5s-99.67-8-99.67,71.39c0,79.61,99.67,76.61,99.67,76.61s250.13-7,250.13,170-32,279,182.09,279,173.09,261,173.09,304c0,146,106,178.5,518.27,178.5V0Z"/></clipPath></defs><title>img1</title><g id="Capa_2" data-name="Capa 2"><g id="Capa_1-2" data-name="Capa 1"><path class="cls-1" d="M433,0,233.42.5s-99.67-8-99.67,71.39c0,79.61,99.67,76.61,99.67,76.61s250.13-7,250.13,170-32,279,182.09,279,173.09,261,173.09,304c0,146,106,178.5,518.27,178.5V0Z"/><g class="cls-2"><image width="1024" height="682" transform="scale(1.33 1.58)" xlink:href="../../istockphoto-1494317561-1024x1024-colorbase.png"/></g></g></g></svg> -->
     </div>
     
