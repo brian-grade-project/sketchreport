3b08c0cffe4e3af05784cfb065baf7357d084351
Brian <brianmedina042@gmail.com>
2024-07-13 00:44:09 +0200

    _lasangredecristotienepoder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,21 @@
 {{-- // TODO: vista para inicio de sesión --}}
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+@section('content')
+    <div class="w-screen h-screen">
+        <div class="h-screen w-2/6 bg-zinc-800">
+            
+        </div>
+    </div>
+@endsection
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+git<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
